1b91f7e726fef26e0f0592c2d1eb2dd3cbdae983
ltlnx <litelinux.org@gmail.com>
2021-01-30 15:55:52 +0800

    update site

--- js.js
+++ js.js
@@ -1,10 +1,10 @@
 var openmenu = {
 	view: function() {
-		return m("main", [
-			m("a.class", {href:"posts.html"}, "貼文"),
-			m("a.class", {href:"about.html"}, "關於我"),
-			m("a.class", {href:"index.html"}, "回首頁"),
-			m("a.class", {href:"#!/hello"}, "關閉選單"),
+		return m("main.open", [
+			m("a.open", {href:"posts.html"}, "貼文"),
+			m("a.open", {href:"about.html"}, "關於我"),
+			m("a.open", {href:"index.html"}, "回首頁"),
+			m("a.open", {href:"#!/hello"}, "關閉選單"),
 			])
 	}
 }
