b80deb9613d48bc70f06a44944e8c3253338eae9
ltlnx <litelinux.org@gmail.com>
2021-01-31 18:14:32 +0800

    update menu links & other stuff

--- js.js
+++ js.js
@@ -1,9 +1,10 @@
+var baseURL = window.location.href.match(/(.*?)\.io/i)[1]+'.io'
 var openmenu = {
 	view: function() {
 		return m("main.open", [
-			m("a.open", {href:"posts/posts.html"}, "貼文"),
-			m("a.open", {href:"about.html"}, "關於我"),
-			m("a.open", {href:"index.html"}, "回首頁"),
+			m("a.open", {href:baseURL+"/posts/posts.html"}, "貼文"),
+			m("a.open", {href:baseURL+"/about.html"}, "關於我"),
+			m("a.open", {href:baseURL+"/index.html"}, "回首頁"),
 			m("a.open", {href:"#!/hello"}, "關閉選單"),
 			])
 	}
